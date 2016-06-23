a2a82402f0b1d93309407469d98d4de50081bbae
Dominic Hickey <dominic.j.hickey@gmail.com>
2016-06-23 10:09:55 +0100

    Converted LESS to Sass

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('concat', function() {
+    return gulp.src(
+        [
+            './**/**/*.scss',
+            './**/*.scss',
+            './*.scss',
+            '!./node_modules/**/*.scss',
+            '!./concat/**/*.scss',
+            '!./validation.scss',
+            '!./bk-sass-precompiled.scss'
+        ]
+    )
+    .pipe(concat('bk-sass-framework.scss'))
+    .pipe(gulp.dest('./concat/'));
+});
